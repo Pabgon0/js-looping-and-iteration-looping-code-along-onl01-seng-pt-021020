91deb1daab0b236b8c91cb3d4022ff26adb72a89
Pablo Gonzalez <pabgon0@gmail.com>
2020-08-11 23:09:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function writeCards(names, event) {
-  for (let i =0; i < names.length; i++);
+  for (let i =0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`)
+  }
 }
